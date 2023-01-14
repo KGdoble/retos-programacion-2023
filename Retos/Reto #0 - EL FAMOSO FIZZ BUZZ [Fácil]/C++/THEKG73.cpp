2cc0dee2db5f6a4dc19097835c24374e8e11fa9f
THEKG73 <59373209+KGdoble@users.noreply.github.com>
2023-01-14 12:47:23 -0400

    Update THEKG73.cpp

--- Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/C++/THEKG73.cpp
+++ Retos/Reto #0 - EL FAMOSO FIZZ BUZZ [Fácil]/C++/THEKG73.cpp
@@ -1,10 +1,10 @@
 /*Reto #0 - EL FAMOSO FIZZ BUZZ
  * Escribe un programa que muestre por consola (con un print) los
- * números de 1 a 100 (ambos incluidos y con un salto de línea entre
- * cada impresión), sustituyendo los siguientes:
- * - Múltiplos de 3 por la palabra "fizz".
- * - Múltiplos de 5 por la palabra "buzz".
- * - Múltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
+ * nï¿½meros de 1 a 100 (ambos incluidos y con un salto de lï¿½nea entre
+ * cada impresiï¿½n), sustituyendo los siguientes:
+ * - Mï¿½ltiplos de 3 por la palabra "fizz".
+ * - Mï¿½ltiplos de 5 por la palabra "buzz".
+ * - Mï¿½ltiplos de 3 y de 5 a la vez por la palabra "fizzbuzz".
  */
 
 #include<iostream>
@@ -13,15 +13,18 @@
 using namespace std;
 
 int main(){
-	for (int i = 0; i <= 100; i++){
+	for (int i = 1; i <= 100; i++){
 		if (i % 3 == 0 && i % 5 == 0){
-			cout<<i<<" = fizzbuzz\n";
+			cout<<"fizzbuzz\n";
 		}
 		else if (i % 3 == 0){
-			cout<<i<<" = fizz\n";
+			cout<<"fizz\n";
 		}
 		else if (i % 5 == 0){
-			cout<<i<<" = buzz\n";
+			cout<<"buzz\n";
+		}
+		else {
+			cout<<i<<endl;
 		}
 		
 	}
